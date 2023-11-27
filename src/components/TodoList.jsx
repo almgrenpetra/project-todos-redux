f1e1cb8e5524fe52acbb1248409982d10c4aeb4d
petrame <63057558+petrame@users.noreply.github.com>
2023-11-28 00:45:45 +0100

    Added Lottie animation

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,20 +2,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { AddTask } from "./AddTask";
 import { Task } from "./Task";
 import { todoList } from "../reducers/todoList";
-import moment from "moment";
+import Lottie from "lottie-react";
+import animationData from "../assets/Animation - 1701085060253.json";
 import "./TodoList.css";
 
 export const TodoList = () => {
   const allTasks = useSelector((store) => store.todoList.tasks);
   const dispatch = useDispatch();
 
-  const date = moment().format("YYYY-MM-DD");
-  const time = moment().format("HH:mm");
-
   return (
     <>
       <div className="todo-list">
         <AddTask />
+        {allTasks.length === 0 && <Lottie animationData={animationData} />}
         {allTasks.map((task) => (
           <Task key={task.id} task={task} />
         ))}
--- src/reducers/todoList.js
+++ src/reducers/todoList.js
@@ -1,12 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  tasks: [
-    { id: 1, text: "Watch video on actions & reducers", complete: true },
-    { id: 2, text: "Follow redux codealong", complete: true },
-    { id: 3, text: "Fork weekly assignment", complete: true },
-    { id: 4, text: "Create a todo app", complete: false },
-  ],
+  tasks: [],
 };
 
 export const todoList = createSlice({
